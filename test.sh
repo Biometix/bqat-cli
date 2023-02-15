7774833acc46832df5bd1b0d127fa38f9e565ada
L.Wu <l.wu@biometix.com>
2023-02-15 16:00:42 +1100

    Modify test config

--- test.sh
+++ test.sh
@@ -1,7 +1,5 @@
 docker run --rm \
     --shm-size=8G \
-    --cpus=8 \
-    --memory=10G \
     -v "$(pwd)"/data:/app/data \
     bqat-cli \
-    "python3.8 -m pytest tests -v"
+    "python3.8 -m pytest tests"
