27cde799af25cdff1827bd83df3c2f332485e494
Liam Wu <l.wu@biometix.com>
2023-11-09 12:26:31 +1100

    feat: add ps script

---
+++ run.ps1
@@ -0,0 +1,12 @@
+$flags = $args
+
+# Check Docker is running
+if ((docker ps 2>&1) -match '^(?!error)'){
+    Write-Host ""
+} else {
+    Write-Host "Please start Docker before running this script"
+    Exit
+}
+
+# Run BQAT-CLI
+docker run --rm -it --shm-size=8G -v $PWD\data:/app/data ghcr.io/biometix/bqat-cli:latest "python3.8 -m bqat $flags"
